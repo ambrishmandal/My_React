0f0d7492aa57fc94f8bdfe872f7077f6a640f842
ambrishmandal <mandalambrish@gmail.com>
2024-10-20 20:01:22 +0530

    All the project

---
+++ Proj_fold/1-todo-app-version-1/src/components/TodoItem1.jsx
@@ -0,0 +1,19 @@
+function TodoItem1() {
+  let todoName = "Go to The park";
+  let todoDate = "4/10/2024";
+  return (
+    <div class="items-container ">
+      <div class="row ">
+        <div class="col-6">{todoName}</div>
+        <div class="col-4">{todoDate}</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger ak-row">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem1;
